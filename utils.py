0e3915464f87cb20fd76e0be483b77900d8ffb58
dizzi <nicofirst1@gmail.com>
2018-02-08 18:39:02 +0100

    updates

--- utils.py
+++ utils.py
@@ -124,8 +124,8 @@ def capture_image(image_name):
 
 
 def capture_video(video_name,seconds):
-    frame_width = 1024
-    frame_height = 720
+    frame_width = 640
+    frame_height = 480
     print(frame_height,frame_width)
     fps=10
     out = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps, (frame_width, frame_height))
