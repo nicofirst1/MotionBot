33655cf63a9af8636e4a855a6bf0f551fe3760a3
dizzi <nicofirst1@gmail.com>
2018-02-09 13:50:24 +0100

    updates

--- Cam.py
+++ Cam.py
@@ -20,11 +20,11 @@ class Cam_class:
 
     def capture_image(self,image_name):
         print("taking image")
+        img = self.frames[-1]
 
-        if self.frames:
-            img = self.frames[-1]
 
-        else:
+
+        if img==0:
             print("empy queue")
             return False
         # try to save the image
