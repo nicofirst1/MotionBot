5b62ec3b3288bd1b3c6445f81144f68c35340b8f
dizzi <nicofirst1@gmail.com>
2018-02-09 14:05:50 +0100

    updates

--- Cam.py
+++ Cam.py
@@ -38,7 +38,7 @@ class Cam_class:
         frame_width = 640
         frame_height = 480
         print(frame_height, frame_width)
-        fps = 100
+        fps = 10
         out = cv2.VideoWriter(video_name, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), fps,
                               (frame_width, frame_height))
 
