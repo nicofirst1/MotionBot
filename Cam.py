aba0a664c5e37615efcf72e104dcfdab3ff3e684
dizzi <nicofirst1@gmail.com>
2018-02-10 11:51:32 +0100

    bug fixed

--- Cam.py
+++ Cam.py
@@ -49,7 +49,7 @@ class Cam_class:
         fps = 20
         print("initializing writer")
 
-        codec=cv2.cv2.VideoWriter_fourcc(*'MPEG') # cv2.VideoWriter_fourcc(*'MP4V')
+        codec=cv2.VideoWriter_fourcc(*'MPEG') # cv2.VideoWriter_fourcc(*'MP4V')
         print("a")
         out = cv2.VideoWriter(video_name, codec, fps,(frame_width, frame_height))
 
