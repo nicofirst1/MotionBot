3cb4cd8fe33080203cc906ebecd352e66d4dfd5d
dizzi <nicofirst1@gmail.com>
2018-02-11 19:30:30 +0100

    bug fixed

--- Cam.py
+++ Cam.py
@@ -382,9 +382,9 @@ class Cam_movement(Thread):
         # dilate the thresholded image to fill in holes, then find contours
         # on thresholded image
         thresh = cv2.dilate(thresh, None, iterations=2)
-        (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-                                     cv2.CHAIN_APPROX_SIMPLE)
-
+        ret= cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+        print(ret)
+        (cnts, _)=ret
         # loop over the contours
         for c in cnts:
             # if the contour is too small, ignore it
