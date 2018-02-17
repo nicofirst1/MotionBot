c818e62d6d930a0e96f1974e4481aadb87b2bbe9
dizzi <nicofirst1@gmail.com>
2018-02-17 17:00:52 +0100

    bug fixed

--- Face_recognizer.py
+++ Face_recognizer.py
@@ -286,7 +286,8 @@ class Face_recognizer(Thread):
 
         # predict the image using our face recognizer
         collector=[]
-        label, confidence = self.face_recognizer.predict(gray,collector,predictedLabel = -1, confidence = 0.0)
+        non_so_cosa=[]
+        label, confidence = self.face_recognizer.predict(gray,collector,non_so_cosa)
         print(label, confidence, collector)
         # get name of respective label returned by face recognizer
         label_text = self.name_from_label(label)
