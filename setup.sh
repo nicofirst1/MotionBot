1963e3e9502b41724c7383f89a7e800a166d2ca2
dizzi <nicofirst1>
2019-02-12 21:04:53 +0100

    update setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,38 @@
 #!/usr/bin/env bash
 
 cwd=$(pwd)
+# create dir if does not exists
+mkdir -p ./Resources/Weights
+
 
 cd Resources/Weights
-echo "downloading weights..."
-wget https://pjreddie.com/media/files/yolov3-openimages.weights
-wget https://pjreddie.com/media/files/yolov3.weights
 
-cd cwd
-cd
+if [ ! -f yolov3-openimages.weights ]; then
+    echo "downloading openimage weights..."
+    wget https://pjreddie.com/media/files/yolov3-openimages.weights
+
+fi
+if [ ! -f yolov3-openimages.weights ]; then
+    echo "downloading coco weights..."
+    wget https://pjreddie.com/media/files/yolov3.weights
+
+fi
+
+cd $cwd
+
+echo "copying custom files"
+
+cp ./Resources/Custom_darknet/coco.data ./src/darknet/cfg/coco.data
+cp ./Resources/Custom_darknet/openimages.data ./src/darknet/cfg/openimages.data
+
+cp ./Resources/Custom_darknet/image.c ./src/darknet/src/image.c
+cp ./Resources/Custom_darknet/image.h ./src/darknet/src/image.h
+
+cp ./Resources/Custom_darknet/Makefile   ./src/darknet/Makefile
+
+cp ./Resources/Custom_darknet/darknet.py   ./src/darknet/python/darknet.py
 
 
-echo "downloading cfgs..."
+cd ./src/darknet/
 
-wget https://github.com/pjreddie/darknet/tree/master/cfg/yolov3-openimages.cfg
-wget https://github.com/pjreddie/darknet/tree/master/cfg/openimages.data
+make
