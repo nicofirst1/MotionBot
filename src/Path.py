9ac5530e92e05478f4157d52ccd4f23968562731
dizzi <nicofirst1>
2019-02-18 19:27:08 +0100

    comment

--- src/Path.py
+++ src/Path.py
@@ -1,47 +1,48 @@
-import shutil
-from os import curdir
 import os
+import shutil
 from os.path import join
 
-class Path:
 
-    curdir=os.getcwd()
-    curdir=curdir.split("MotionBot")[0]
-    curdir=join(curdir,"MotionBot")
+class Path:
+    curdir = os.getcwd()
+    curdir = curdir.split("MotionBot")[0]
+    curdir = join(curdir, "MotionBot")
 
     RESOURCES_DIR = join(curdir, "Resources")
     LOGGER_DIR = join(RESOURCES_DIR, "Loggers")
-    WEIGHTS_DIR=join(RESOURCES_DIR,"Weights")
-    CUSTOM_DARK_DIR=join(RESOURCES_DIR,"Custom_darknet")
-    ANALISYS_DIR=join(RESOURCES_DIR,"Analisys")
-    recognizer=join(RESOURCES_DIR,"recognizer.yaml")
+    WEIGHTS_DIR = join(RESOURCES_DIR, "Weights")
+    CUSTOM_DARK_DIR = join(RESOURCES_DIR, "Custom_darknet")
+    ANALISYS_DIR = join(RESOURCES_DIR, "Analisys")
+    recognizer = join(RESOURCES_DIR, "recognizer.yaml")
 
-    model=join(RESOURCES_DIR,"model.pkl")
-    time_profiler=join(RESOURCES_DIR,"time_profiler")
-    token_psw=join(RESOURCES_DIR,"token_psw.txt")
-    ids=join(RESOURCES_DIR,"ids")
-    encodings="encodings.pkl"
+    model = join(RESOURCES_DIR, "model.pkl")
+    time_profiler = join(RESOURCES_DIR, "time_profiler")
+    token_psw = join(RESOURCES_DIR, "token_psw.txt")
+    ids = join(RESOURCES_DIR, "ids")
+    encodings = "encodings.pkl"
 
-    DARKNET_DIR=join(curdir,"src/darknet")
+    DARKNET_DIR = join(curdir, "src/darknet")
 
-
-
-    FACES_DIR=join(curdir,"Faces")
-    UNK_DIR=join(FACES_DIR,"Unknown")
+    FACES_DIR = join(curdir, "Faces")
+    UNK_DIR = join(FACES_DIR, "Unknown")
 
     def __init__(self):
         self.initialize_dirs()
 
-        self.create_files([self.ids,self.token_psw])
-
+        self.create_files([self.ids, self.token_psw])
 
     def create_files(self, files):
+        """
+        Create files if not present
+        :param files: list of paths to files
+        :return:
+        """
 
         for f in files:
             try:
-                 open(f, 'r')
+                open(f, 'r')
             except IOError:
-                 open(f, 'w')
+                open(f, 'w')
 
     def initialize_dirs(self):
         """
@@ -73,8 +74,8 @@ class Path:
                     print(e)
 
     @staticmethod
-    def join(path1,path2):
-        return join(path1,path2)
+    def join(path1, path2):
+        return join(path1, path2)
 
 
 if __name__ == '__main__':
